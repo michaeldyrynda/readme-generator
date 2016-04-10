64620cfd984a1a7b95d7d5a214846f1f607e73b8
Michael Dyrynda <michael@dyrynda.com.au>
2016-04-10 20:20:51 +0930

    Add clipboard functionality
    Ensure we bind nl2br filtering two-ways
    Update fetch markdown button label

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,19 @@
 var Vue = require('vue'),
     _ = require('underscore'),
     marked = require('marked'),
-    modal = require('vue-strap/dist/vue-strap.min').modal;
+    modal = require('vue-strap/dist/vue-strap.min').modal,
+    Clipboard = require('clipboard');
 
 Vue.filter('nl2br', {
 
     write(value) {
-
+        if (value && value.constructor === Array) {
+            return value.join('\n\n');
+        }
     },
     
     read(value) {
-        if (value.constructor === Array) {
+        if (value && value.constructor === Array) {
             return value.join('\n\n');
         }
     }
@@ -18,10 +21,15 @@ Vue.filter('nl2br', {
 });
 
 new Vue({
+
     el: '#app',
+
     components: { modal },
+
     data: {
+
         markdownSource: {
+
             title: '# My new project',
             headingIntro: "## Introduction",
             introduction: "> An introduction or lead on what problem you're solving. Answer the question, \"Why does someone need this?\"",
@@ -29,17 +37,41 @@ new Vue({
             codeSamples: "> You've gotten their attention in the introduction, now show a few code examples. So they get a visualization and as a bonus, make them copy/paste friendly.",
             headingInstallation: "## Installation",
             installation: "> The installation instructions are low priority in the readme and should come at the bottom. The first part answers all their objections and now that they want to use it, show them how.",
+
         },
 
         generatedMarkdown: "",
 
         showMarkdownModal: false,
+
+        outputCopied: false,
+
+        copiedMessage: '',
+
     },
+
+    ready() {
+
+        new Clipboard('.clipper').on('success', (e) => {
+            this.outputCopied = true;
+            this.copiedMessage = 'Copied!';
+        }).on('error', (e) => {
+            this.outputCopied = true;
+            this.copiedMessage = 'Press Cmd+C to copy';
+        });
+
+    },
+
     filters: {
+
         marked: marked
+
     },
+
     methods: {
+
         fetchRawMarkdown() {
+
             if (this.markdownSource.title.substring(0, 1) !== '#') {
                 this.markdownSource.title = '# ' + this.markdownSource.title;
             }
@@ -49,8 +81,17 @@ new Vue({
             });
 
             this.showMarkdownModal = true;
+        },
+
+        closeModal() {
+
+            this.showMarkdownModal = false;
+            this.outputCopied = false;
+
         }
+
     }
+
 });
 
 // Handling to make sure you a tab inserts four spaces in the text area.
