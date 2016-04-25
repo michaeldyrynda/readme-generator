bd0cb37675fbca4bde82d2512a0d5b30b6b85ab3
Michael Dyrynda <michael@dyrynda.com.au>
2016-04-25 15:39:00 +0930

    update structure to accommodate GitHub pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var elixir = require('laravel-elixir');
 require('laravel-elixir-vueify');
 
 elixir(function (mix) {
-    mix.sass('app.scss')
-        .browserify('app.js')
+    mix.sass('app.scss', 'build/app.css')
+        .browserify('app.js', 'build/app.js')
         // Serve the app from base dir and php -S 0.0.0.0:8888 -t public
         .browserSync({ proxy: 'http://localhost:8888' }); 
 });
